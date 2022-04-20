e49ebe40b91d157af84533732fe82ea5df00c416
rounak-dev <rounak.infymax@gmail.com>
2022-04-20 12:30:19 +0530

    html - css - auth - routing in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+
+/*-- importing routes --*/
+import { Routes, RouterModule } from "@angular/router";
+
+/*--create a variable to store all routes and has specif type Routes--*/
+const appRoutes: Routes = [{
+  path: 'login', component: AuthComponent
+}];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
